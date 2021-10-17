a432e36fa26e6b9756ef9b708db9714d4534cc93
Tiunchik <senebh@gmail.com>
2021-10-17 17:03:30 +0300

    Second

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,141 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+interface Props {
+    value: string,
+    onClick: () => void
+}
+
+interface State {
+    value: string
+}
+
+interface ArrayState {
+    squares: Array<any>,
+    xIsNext: boolean
+}
+
+interface History {
+    squares: Array<{square: Array<string>}>,
+    xIsNext: boolean
+}
+
+function Square(props: Props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props?.value}
+        </button>
+    );
+}
+
+class Board extends React.Component<{}, ArrayState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            squares: [{ square: Array(9).fill(null) }],
+            xIsNext: true
+        }
+    }
+
+    renderSquare(i: number) {
+        return <Square value={this.state.squares[i]}
+                       onClick={() => this.handleClick(i)}/>;
+    }
+
+    render() {
+        const winner = this.calculateWinner(this.state.squares)
+        let status;
+        if (winner) {
+            status = 'Winner is  ' + winner;
+        } else {
+            status = 'Next step: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+
+
+    handleClick(i: number) {
+        const squares = this.state.squares.slice()
+        if (this.calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState(
+            {
+                squares: squares,
+                xIsNext: !this.state.xIsNext
+            })
+    }
+
+    calculateWinner(squares: Array<any>) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+}
+
+class Game extends React.Component<{}, History> {
+    constructor(props: History) {
+        super(props);
+        this.state = {
+            squares: [{Array(9).fill(null)}]
+        }
+    }
+
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game/>,
+    document.getElementById('room')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
